bb8210e5dcda3ebc8885e24eb3bc932c2e0ff598
Gervásio Júnior <juniorb08@gmail.com>
2018-03-20 11:44:07 -0300

    Adiciona app com sockets

---
+++ simple_app_socket/server.js
@@ -0,0 +1,8 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+io.on('connection', socket => socket.on('chat message', msg => io.emit('chat message', msg)))
+
+http.listen(8000)
